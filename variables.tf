1f8076e247c627b306b43b6a064de024720df9d2
Gheorghe Isac <gheorghe_isac@smart-x.net>
2022-12-14 17:36:15 +0100

    add extra user_data

--- variables.tf
+++ variables.tf
@@ -252,6 +252,12 @@ variable "retry_queue_interval" {
   }
 }
 
+variable "extra_user_data" {
+  description = "Extra user data to run"
+  default     = ""
+  type        = string
+}
+
 # --- Iglu Resolver
 
 variable "default_iglu_resolvers" {
